0bc96667d2d16d4f747e65b10218200da9994690
rkcmorley <rkcmorley@gmail.com>
2024-01-15 21:12:10 +0000

    "Edited the two files"

---
+++ Spread Operator/spread_operator.js
@@ -0,0 +1,13 @@
+// Spread Operator
+
+var array = ["a", "b", "c"];
+console.log(array);
+
+// array starts at 0
+console.log(array[0]); // a
+console.log(array[1]); // b
+
+// push a value into the array
+array.push("d");
+console.log(array);
+
