ed140ee75d2109640b678d1aad61feeee448e557
rkcmorley <rkcmorley@gmail.com>
2024-01-28 15:02:52 +0000

    Added more codes in Promise

--- Promise/promise.js
+++ Promise/promise.js
@@ -2,7 +2,6 @@
 // Promise requires two parameters, resolve and reject.
 // Resolves is successful asynchronous call.
 // Reject is failed asynchronous call.
-
 function checkIfSuccessful(data) {
     return new Promise(function (resolve, reject) {
         if (data === "Success") {
@@ -20,4 +19,21 @@ checkIfSuccessful("Success").then(function (result) {
     console.log(result);
 }).catch(function (error) {
     console.log(error)
-})
+});
+
+/**
+ * The fetch() function in JavaScript is used to make network requests
+ * to fetch resources, such as JSON data, from a server. 
+ **/
+// This is another asynchronous call.
+// To receive data, use then() method.
+// then() method uses callback functions.
+// You can have multiple promises, such as .json and fetch() method.
+fetch('https://api.github.com/users/rkcmorley').then(function (result) {
+    console.log(result); // This will show the object, it's my user profile.
+    return result.json(); // This will return the object in JSON format.
+}).then(function (response) {
+    console.log(response); // This will show the object in JSON format. Which is my user profile.
+}).catch(function (error) {
+    console.log(error)
+});
